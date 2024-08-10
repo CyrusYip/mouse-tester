18b96c6367908893efcef8ad957a99b06158653d
Cyrus Yip <60951091+CyrusYip@users.noreply.github.com>
2024-08-10 22:38:08 +0800

    feat: scroll test

--- src/App.jsx
+++ src/App.jsx
@@ -22,8 +22,13 @@ function App() {
     <>
       <div className='flex flex-col items-center justify-center min-h-dvh'>
         <h1 className="text-3xl font-bold">Mouse Tester</h1>
-        <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-3 px-6 rounded shadow-lg" onMouseDown={handleMouseDown} onContextMenu={handleContextMenu}>Click me</button>
+        <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-3 px-6 rounded shadow-lg" onMouseDown={handleMouseDown} onContextMenu={handleContextMenu}>Click test</button>
         <p>Clicked button: {clickEvent.button}</p>
+        <div className='border h-[100px] mt-8 overflow-y-auto'>
+          <p className='text-center font-bold text-blue-500'>Scroll test</p>
+          <div className='h-[1000px]'></div>
+          <p className='text-center font-bold text-blue-500'>This is the end</p>
+        </div>
       </div>
     </>
   )
