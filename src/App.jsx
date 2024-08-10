3cfe570632fb57daf0aeda38cec0e06116f46f82
Cyrus Yip <60951091+CyrusYip@users.noreply.github.com>
2024-08-10 23:29:10 +0800

    feat: click count

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,27 @@ import { useEffect } from 'react'
 import { useState } from 'react'
 
 function App() {
-  const [clickEvent, setClickEvent] = useState(0)
+  const [buttonCount, setButtonCount] = useState({
+    left: 0,
+    middle: 0,
+    right: 0,
+    back: 0,
+    forward: 0,
+  })
+  const buttonMap = {
+    0: "left",
+    1: "middle",
+    2: "right",
+    3: "back",
+    4: "forward",
+  }
   const handleMouseDown = e => {
-    setClickEvent(e)
+    const button = buttonMap[e.button.toString()]
+    const count = buttonCount[button]
+    setButtonCount({
+      ...buttonCount,
+      [button]: count + 1
+    })
   }
   const handleContextMenu = e => {
     // Disable context menu
@@ -20,42 +38,40 @@ function App() {
 
   return (
     <>
-      <div className='flex flex-col items-center justify-center min-h-dvh'>
-        <button className='bg-blue-500 hover:bg-blue-600 text-white font-semibold py-3 px-6 rounded shadow-lg' onMouseDown={handleMouseDown} onContextMenu={handleContextMenu}>Click test</button>
-        <p>Clicked button: {clickEvent.button}</p>
-
-        <table className='border'>
+      <div className='flex flex-col items-center justify-center min-h-dvh py-4'>
+        <button className='bg-blue-500 hover:bg-blue-600 text-white font-semibold py-3 px-6 rounded' onMouseDown={handleMouseDown} onContextMenu={handleContextMenu}>Click test</button>
+        <table className='border mt-1'>
           <thead>
             <tr>
-              <th scope="col" className='border'>Buttons</th>
-              <th scope="col" className='border'>Status</th>
+              <th scope="col" className='border text-left'>Button</th>
+              <th scope="col" className='border text-left'>Count</th>
             </tr>
           </thead>
           <tbody>
             <tr>
               <td className='border' title='Right button on left-handed mouse'>Left</td>
-              <td className='border'>✓</td>
+              <td className='border'>{buttonCount.left}</td>
             </tr>
             <tr>
               <td className='border'>Middle</td>
-              <td className='border'></td>
+              <td className='border'>{buttonCount.middle}</td>
             </tr>
             <tr>
               <td className='border' title='Left button on left-handed mouse'>Right</td>
-              <td className='border'></td>
+              <td className='border'>{buttonCount.right}</td>
             </tr>
             <tr>
               <td className='border'>Back</td>
-              <td className='border'></td>
+              <td className='border'>{buttonCount.back}</td>
             </tr>
             <tr>
               <td className='border'>Forward</td>
-              <td className='border'></td>
+              <td className='border'>{buttonCount.forward}</td>
             </tr>
           </tbody>
         </table>
 
-        <div className='border h-[100px] mt-8 overflow-y-auto'>
+        <div className='border h-[100px] mt-8 px-2 overflow-y-auto'>
           <p className='text-center font-bold text-blue-500'>Scroll test</p>
           <div className='h-[1000px]'></div>
           <p className='text-center font-bold text-blue-500'>This is the end</p>
